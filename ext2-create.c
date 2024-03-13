ca8a85491ed437439299d9d149a9b6282f5a0d91
TIRUMALAI <tirumala@lnxsrv15.seas.ucla.edu>
2024-03-13 08:27:25 -0700

    Update

--- ext2-create.c
+++ ext2-create.c
@@ -396,10 +396,10 @@ void write_inode_table(int fd) {
 
 	struct ext2_inode helloworld_inode ={0};
 	helloworld_inode.i_mode = EXT2_S_IFREG
-	                    	   | EXT2_S_IRUSR
-	                           | EXT2_S_IWUSR
-	                           | EXT2_S_IRGRP
-	                           | EXT2_S_IROTH;
+								| EXT2_S_IRUSR
+								| EXT2_S_IWUSR
+								| EXT2_S_IRGRP
+								| EXT2_S_IROTH;
 	helloworld_inode.i_uid = 1000;
 	helloworld_inode.i_size = 12;
 	helloworld_inode.i_atime = current_time;
